9ed453232844bf9614b8a97f3e2271a611f54a3b
fhwang15 <fhwang@inside.artcenter.edu>
2024-03-26 04:17:36 +0900

    Rough mvment
    -- Very Very Rough

--- RhythmRPG/Assets/Script/MovingNote.cs
+++ RhythmRPG/Assets/Script/MovingNote.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MovingNote : MonoBehaviour
 {
+
+    public float force;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,16 @@ public class MovingNote : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        force = Time.deltaTime*3;
+        transform.Translate(Vector3.left*force);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "PreparedNote")
+        {
+            Destroy(gameObject);
+        }
     }
 }
